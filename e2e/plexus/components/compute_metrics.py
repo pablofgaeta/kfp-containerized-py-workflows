5a98b31bea180c28eceaf78a48bb223f2d80f5cc
Pablo Gaeta <pablogaeta@google.com>
2023-10-29 12:47:17 -0500

    Fixed bug in file saving/loading

--- e2e/plexus/components/compute_metrics.py
+++ e2e/plexus/components/compute_metrics.py
@@ -7,30 +7,35 @@ import yaml
 config_path = os.path.join(os.path.dirname(__file__), "..", "user", "config.yaml")
 with open(config_path, "r") as f:
     user_config: dict = yaml.safe_load(f)
+    component_config: dict = user_config["component_config"]
 
 default_config = {
     "base_image": "python:3.9",
-    "packages_to_install": [],
+    "packages_to_install": ["pandas", "gcsfs", "fastparquet"],
     "target_image": None,
 }
 
-component_config: dict = user_config.get("compute_metrics", default_config)
-
+base_image = component_config.get('base_image', default_config["base_image"])
+packages_to_install = list(set(default_config["packages_to_install"] + component_config.get('packages_to_install', [])))
+target_image = component_config.get("target_image", default_config["target_image"])
 
 @dsl.component(
-    base_image=component_config.get("base_image"),
-    packages_to_install=component_config.get("packages_to_install"),
-    target_image=component_config.get("target_image"),
+    base_image=base_image,
+    packages_to_install=packages_to_install,
+    target_image=target_image,
 )
 def compute_metrics(sample_dataset: Input[Dataset], mean_value: Output[Metrics]):
-    import numpy as np
+    import logging
+    import pandas as pd
 
     from user import stats
 
-    import gcsfs
+    try:
+        sample_dataset_path = sample_dataset.path
 
-    fs = gcsfs.GCSFileSystem()
-    with fs.open(sample_dataset.path, "r") as sample_file:
-        sample = np.load(sample_file)
+        logging.info(f"Reading dataset from path: {sample_dataset_path}")
+        sample_df = pd.read_parquet(sample_dataset_path, engine="fastparquet")
 
-    mean_value.log_metric("mean", stats.sample_mean(sample))
+        mean_value.log_metric("mean", stats.sample_mean(sample_df['sample']))
+    except Exception as e:
+        logging.exception(e)
--- e2e/plexus/runner.py
+++ e2e/plexus/runner.py
@@ -3,32 +3,41 @@ import os
 import yaml
 
 import kfp
-from pipeline import test_multi_component_pipeline
+from pipelines.python_containerized_multi import python_containerized_multi
 
 from google.cloud import aiplatform
 
 # %%
+DISPLAY_NAME = "py-con-multi-test"
 CONFIG_PATH = os.path.join(os.path.dirname(__file__), "build", "user", "config.yaml")
-PIPELINE_SPEC_FILE = os.path.join(os.path.dirname(__file__), "pipeline.yaml")
+PIPELINE_SPEC_FILE = os.path.join(os.path.dirname(__file__), "pipelines", "pipeline.yaml")
 
 # %%
+# replace with default config in application
+default_config = {
+    "location": "us-central1",
+    "enable_caching": False,
+    "parameter_values": {
+        "n": 10
+    }
+}
+
 with open(CONFIG_PATH, "r") as f:
     user_config: dict = yaml.safe_load(f)
+    pipeline_config = default_config
+    pipeline_config.update(user_config.get("pipeline_config", {}))
 
 # %%
-kfp.compiler.Compiler().compile(test_multi_component_pipeline, PIPELINE_SPEC_FILE)
+kfp.compiler.Compiler().compile(python_containerized_multi, PIPELINE_SPEC_FILE)
 
 # %%
 job = aiplatform.PipelineJob(
-    display_name="python-containerized-multi-test",
     template_path=PIPELINE_SPEC_FILE,
-    enable_caching=False,
-    pipeline_root="gs://cvs_demo_bucket",
-    parameter_values=user_config.get("config"),
-    project="pg-cvs-sandbox",
-    location="us-west2",
+    **pipeline_config
 )
 
 # %%
-job.submit()
+job.submit(
+    service_account="cvs-vai-pipeline@pg-cvs-sandbox.iam.gserviceaccount.com"
+)
 # https://pantheon.corp.google.com/vertex-ai/locations/us-west2/pipelines/runs/python-containerized-test-20231025152534?project=pg-cvs-sandbox&e=13803378&mods=dm_deploy_from_gcs
